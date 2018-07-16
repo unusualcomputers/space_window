163869ba7e6d55da79fe90d32e0eae95a340f4a8
unusualcomputers <unusual.computers@gmail.com>
2018-07-16 08:02:14 +0100

    logging fixing

--- code/logger.py
+++ code/logger.py
@@ -1,7 +1,22 @@
 import logging
-
-logging.basicConfig(level=logging.DEBUG, 
-    format='%(asctime)s: %(levelname)s: %(name)s-%(threadName)s - %(message)s')
-
-def get(name):
-    return logging.getLogger(name)
+import sys
+loggers={}
+logfrmt='%(levelname)s - %(asctime)s:%(msecs)d - %(name)s: %(message)s'
+formatter=logging.Formatter(logfrmt,'%H:%M:%S')
+lvl=logging.DEBUG
+console = logging.StreamHandler(sys.stdout)
+console.setLevel(lvl)
+console.setFormatter(formatter)
+rootLog='SpaceWindow'
+#logging.basicConfig(format=logfrmt,level=lvl)
+def get(subname):
+    global loggers
+    name='%s.%s' %(rootLog,subname)
+    if name in loggers: return loggers[name]
+    
+    l=logging.getLogger(name)
+    l.handlers = []
+    l.addHandler(console) 
+    l.setLevel(lvl)
+    loggers[name]=l
+    return l
