8f18db6dc4b5be3ee4e486de177c25cb787ce539
unusualcomputers <unusual.computers@gmail.com>
2018-08-19 11:06:40 +0100

    typos..

--- code/server.py
+++ code/server.py
@@ -167,7 +167,7 @@ class SpaceWindowServer(BaseHTTPRequestHandler):
             chunk_percent=float(chunk_size)/total_size
             total_loaded=0
             percent=0
-            video_filename=os.path.join(p,filename
+            video_filename=os.path.join(p,filename)
             with file(video_filename), "wb") as videoout:
                 videoin = form['video'].file
                 while True:
