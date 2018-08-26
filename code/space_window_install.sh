60202fed39716c0ff2dca133a0712ede544b6780
unusualcomputers <unusual.computers@gmail.com>
2018-08-26 21:56:56 +0100

    mopidy update

--- code/space_window_install.sh
+++ code/space_window_install.sh
@@ -9,7 +9,11 @@ apt-get install python-pygame
 
 # radio rough - this installs mopoidy and a bunch of its dependencies
 wget https://raw.githubusercontent.com/unusualcomputers/unusualcomputers/master/code/mopidy/mopidyradioroughhtml/rasp_radio_rough_install_lite.sh
+chmod a+x ./rasp_radio_rough_install_lite.sh
 ./rasp_radio_rough_install_lite.sh
+
+# make mopidy server available
+sed -i 's|#hostname = 127.0.0.1|hostname=0.0.0.0|g' /root/.config/mopidy/mopidy.conf
 
 # mopidy json client
 pip install https://github.com/ismailof/mopidy-json-client/archive/master.zip
