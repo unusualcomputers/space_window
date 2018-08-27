72476c2a1d9afedf8536459836456e99fc8b8876
unusualcomputers <unusual.computers@gmail.com>
2018-08-27 14:46:16 +0100

    fixed mopidy client

--- code/mopidy_listener.py
+++ code/mopidy_listener.py
@@ -5,6 +5,23 @@ import logger
 import subprocess
 
 _log=logger.get(__name__)
+
+class FixedMopidyClient(MopidyClient):
+    # current version of json mopidy client has a bug in 
+    # signature of _server_response
+    def __init__(self,ws_url):
+        MopidyClient.__init__(self,ws_url=ws_url)
+
+
+    def _server_response(self, ws, message=None):
+        try:
+            if message is None:
+                ResponseMessage.parse_json_message(ws)
+            else:
+                ResponseMessage.parse_json_message(message)
+        except Exception as ex:
+            _log.exception(ex)
+
 class MopidyUpdates:
     def __init__(self,updates_func):
         ps=run('ps -e')
@@ -13,7 +30,7 @@ class MopidyUpdates:
         ip=wifi.get_ip()
         ws_url='ws://%s:6680/mopidy/ws' % ip
         self._update_func=updates_func
-        self._mopidy=MopidyClient(ws_url=ws_url)
+        self._mopidy=FixedMopidyClient(ws_url=ws_url)
         self._mopidy.bind_event('track_playback_started', self.playback_started)
         self._mopidy.bind_event('stream_title_changed', self.title_changed)
         self._show_updates=False
